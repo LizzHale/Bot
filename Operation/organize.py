017e5c0974d00167d9bf4506ebc56f273c29e566
Elizabeth Hale <lizzhale@mac.com>
2014-03-18 12:42:43 -0700

    Working function that returns an ordered list of unique words in corpus

--- Operation/organize.py
+++ Operation/organize.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+import nltk
+
 import normalize
 
-#create a list of cleaned and tokenized tuples for both neg and pos 
+# returns a list of cleaned and tokenized tuples for both neg and pos 
 def list_of_statements():
     alist = []
 
@@ -11,7 +13,7 @@ def list_of_statements():
             # makes lowercase, expands contractions, 
             # removes punctuation and normalizes whitespace
             negative = normalize.clean(line)
-            negative = tokenize(negative)
+            negative = normalize.tokenize(negative)
             alist.append((negative, "negative"))
 
     with open("../Datasets/pos.txt") as pos:
@@ -21,6 +23,29 @@ def list_of_statements():
             alist.append((positive, "positive"))
 
     return alist
+
+# takes a list of cleaned and tokenized tuples (annotated either positive or negative)
+# and returns a list of all the words
+def get_words_in_corpus(tupledList):
+    all_words = []
+    for (words, sentiment) in tupledList:
+        all_words.extend(words)
+    return all_words
+
+# returns an ordered list (first by frequency then alphabetically) of unique words
+def get_word_features(wordList):
+    # FreqDist returns a dictionary key - word value - frequency
+    wordList = nltk.FreqDist(wordList)
+    word_features = wordList.keys()
+    return word_features
+
+def main():
+    return get_word_features(get_words_in_corpus(list_of_statements()))
+
+
+if __name__ =='__main__':
+    main()
+
 
 
 
