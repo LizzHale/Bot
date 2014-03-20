159cc02006da0048066e4064837dd3ed034bc4b7
Elizabeth Hale <lizzhale@mac.com>
2014-03-19 17:00:38 -0700

    Added function to calculate accuracy - currently at 60%

--- Operation/setupdata.py
+++ Operation/setupdata.py
@@ -19,10 +19,19 @@ def train():
         cl.train(each[0], each[1])
     return cl
 
+def accuracy(classifier, gold):
+    results = classifier.classify_many([fs for (fs,l) in gold])
+    correct = [l==r for ((fs,l), r) in zip(gold, results)]
+    if correct:
+        return float(sum(correct))/len(correct)
+    else:
+        return 0
+
 def test():
     cl = train()
     negative = getlist("negative")
     positive = getlist("positive")
     testing_data = negative[len(negative)/2:]
     testing_data.extend(positive[len(positive)/2:])
-    
+    return accuracy(cl, testing_data)
+
