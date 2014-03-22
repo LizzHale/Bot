f9baa8731d9fe2bcf54d19a7ef6baa40f8ce6a10
Elizabeth Hale <lizzhale@mac.com>
2014-03-22 09:24:42 -0700

    Bot now responds to private messages.

--- Operation/bot.py
+++ Operation/bot.py
@@ -12,6 +12,10 @@ class bot:
         if msg[1]=="PRIVMSG" and msg[2]==self.channel:
             return self.classification(msg[3])
 
+        elif msg[1]=="PRIVMSG" and msg[2]==self.nickname:
+            senderNick = msg[0].split("!", 1)[0].strip(":")
+            return self.private_message(msg[3], senderNick)
+
         elif msg[0]=="PING":
             return self.pong(msg)
             
@@ -38,21 +42,6 @@ class bot:
     def pong(self, msg):
         return "PONG %s\r\n" % msg[1]
 
-       # # when a message to the channel is received, the bot will say hello
-            # # TO DO obviously I don't want it responding "hello" every single time
-            # if line[1]=="PRIVMSG" and line[2]==CHANNEL:
-            #     # Added quit command to shutdown the bot
-            #     if line[3]==":quit":
-            #         s.send("QUIT \r\n")
-            #     else:
-            #         msg = "hello"
-            #         s.send("PRIVMSG %s %s\r\n" % (CHANNEL, msg))
-
-            # # Can respond to "private messages" and respond directly back to the speaker
-            # if line[1]=="PRIVMSG" and line[2]==NICK:
-            #     # line 0 is the originator parameter
-            #     # in this next line, we split on ! taking the left half
-            #     # and then strip the preceeding colon to get the sender's nickname
-            #     senderNick = line[0].split("!", 1)[0].strip(":")
-            #     msg = "hello"
-            #     s.send("PRIVMSG %s %s\r\n" % (senderNick, msg))
+    def private_message(self, msg, sender):
+        reply = "Why are we talking in here?"
+        return "PRIVMSG %s :%s\r\n" % (sender, reply)
