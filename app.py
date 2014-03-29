48dc9245bf1c566aef5720af7212e6b4789444e0
Elizabeth Hale <lizzhale@mac.com>
2014-03-29 12:10:14 -0700

    Cleaning up templating errors

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def welcome():
     # Mock up is based on the fisher classifier
     message = "Despite the rave reviews, the restaurant was terrible"
     features = {'rave': 1, 'reviews': 1, 'restaurant': 1, 'terrible': 1, 'despite': 1}
-    probability_of_positive = 0.659065812937*100 + "%"
-    probability_of_negative = 0.68747796392*100 + "%"
+    probability_of_positive = 0.659065812937*100
+    probability_of_negative = 0.68747796392*100
     classification = "negative"
     return render_template("kiwi.html", message=message, features=features, 
                             probability_of_negative=probability_of_negative, 
