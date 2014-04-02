7d8de2363402dd8a921dee0a5e1517fe5608e59c
Elizabeth Hale <lizzhale@mac.com>
2014-04-02 11:56:18 -0700

    setting up config

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 # Config file, put all your keys and passwords and whatnot in here
 DB_URL = os.environ.get("DATABASE_URL", "sqlite://bob.db")
-SECRET_KEY = "this should be a secret"
+SECRET_KEY = os.environ.get("SECRET_KEY")
