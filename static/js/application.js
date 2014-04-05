9273881252b9ad969265675ec0dcffca917cb887
Elizabeth Hale <lizzhale@mac.com>
2014-04-05 07:03:35 -0700

    Loading graffic

--- static/js/application.js
+++ static/js/application.js
@@ -1,14 +1,16 @@
 $(document).ready( function(){
     $("li.quote").on("click", function(event) {
         quote = $(this).find('blockquote')[0].innerHTML
+        $("#choices").slideToggle();
+        $("#quote_loading").slideToggle();
         $.ajax({
             url: "/classifiers/compare",
             data: {message:quote},
             method: "POST"
         }).done(function(data) {
             console.log("server sent us: " + data);
-            $("#choices").slideToggle();
             $("#retrieved")[0].innerHTML = data;
+            $("#quote_loading").slideToggle();
             $("#retrieved").slideToggle();
         }).fail(function() {
             console.log("you failed")
@@ -21,18 +23,23 @@ $(document).ready( function(){
     $(".btn").on("click", function(event) {
         event.preventDefault();
         message = $("textarea").val();
-        console.log(message)
+        $("#fisher_submit").slideToggle();
+        $("#stats_loading").slideToggle();
         $.ajax({
             url: "/classifiers/stats",
             data: {stats:message},
             method: "POST"
         }).done(function(data) {
             console.log("server sent us: " + data);
-            $("#fisher_submit").slideToggle();
             $("#fisher_details")[0].innerHTML = data;
+            $("#stats_loading").slideToggle();
             $("#fisher_details").slideToggle();
         }).fail(function(){
             console.log("you failed");
         })
+    });
+    $("#fisher_details").on("click", function() {
+        $("#fisher_submit").slideToggle();
+        $("#fisher_details").slideToggle();
     });
 });
