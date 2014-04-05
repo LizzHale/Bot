03663905bd29d8f95aa1d4896f634019fd196659
Elizabeth Hale <lizzhale@mac.com>
2014-04-04 18:04:22 -0700

    text form

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
 def classifiers():
     return render_template("classifiers.html")
 
-@app.route("/classifiers", methods=["POST"])
+@app.route("/classifiers/compare", methods=["POST"])
 def compare():
     message = request.form.get("message")
     details = setupdata.comparison(message)
@@ -30,17 +30,23 @@ def compare():
     return render_template("retrieved.html", message=message, features=features, 
                             bayesclassification=bayesclassification, fisherclassification=fisherclassification)
 
-@app.route("/chat")
-def chat():
-    details = setupdata.stats("Despite the rave reviews, the restaurant was terrible")
+@app.route("/classifiers/stats", methods=["POST"])
+def stats():
+    retrieved = request.form.get("stats")
+    details = setupdata.stats(retrieved)
+    print details
     message = details["message"]
     features = details["features"]
     negprobability = details["docprob"]["negative"]
     posprobability = details["docprob"]["positive"]
     classification = details["classification"]
-    return render_template("chat.html", message=message, features=features, 
+    return render_template("stats.html", message=message, features=features, 
         negprobability=negprobability, posprobability=posprobability, 
         classification=classification)
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
 
 @app.route("/maker")
 def maker():
--- static/js/application.js
+++ static/js/application.js
@@ -1,10 +1,8 @@
 $(document).ready( function(){
     $("li.quote").on("click", function(event) {
         quote = $(this).find('blockquote')[0].innerHTML
-        
-        console.log("you clicked on this quote: " + quote)
         $.ajax({
-            url: "/classifiers",
+            url: "/classifiers/compare",
             data: {message:quote},
             method: "POST"
         }).done(function(data) {
@@ -19,5 +17,22 @@ $(document).ready( function(){
     $("#retrieved").on("click", function() {
         $("#choices").slideToggle();
         $("#retrieved").slideToggle();
+    });
+    $(".btn").on("click", function(event) {
+        event.preventDefault();
+        message = $("textarea").val();
+        console.log(message)
+        $.ajax({
+            url: "/classifiers/stats",
+            data: {stats:message},
+            method: "POST"
+        }).done(function(data) {
+            console.log("server sent us: " + data);
+            $("#fisher_submit").slideToggle();
+            $("#fisher_details")[0].innerHTML = data;
+            $("#fisher_details").slideToggle();
+        }).fail(function(){
+            console.log("you failed");
+        })
     });
 });
