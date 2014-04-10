b74fbca8d93ddcba1f268845dc9074146109f8df
Elizabeth Hale <lizzhale@mac.com>
2014-04-10 13:51:11 -0700

    cleaning

--- test_normalize.py
+++ test_normalize.py
@@ -157,14 +157,3 @@ class TestNormalizeFunctions(unittest.TestCase):
 if __name__ == '__main__':
         unittest.main()
  
-
-# Hook will work because the process will exit with a code and then a shell script will tell it 
-# how to behave based on that. 
-""" Test driven development for complex features -- 
-perhaps use this for the last feature I write
-Red, green, refactor - first have a failing test case, then write it to make it green, then refactor
-Regression side of testing is to avoid being slowed down by bugs. So test the next thing you are 
-going to change.
-Tests are often called specs because they are a form of documentation.
-In memory databases will allow you test but not "commit" the test processes.
-"""
